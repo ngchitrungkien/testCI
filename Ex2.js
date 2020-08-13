282f3974102f227deca879232d2f91a56931e14a
Nguyễn Chí Trung Kiên <41412023+ngchitrungkien@users.noreply.github.com>
2020-08-13 21:57:46 +0700

    Add files via upload

--- Ex2.js
+++ Ex2.js
@@ -1,11 +1,15 @@
-var array1 = ["s", "u", "p", "e", "r"],
-    array2 = ["t", "o", "w", "e", "r"],
-    result = [],
-    i, l = Math.min(array1.length, array2.length);
-       
-for (i = 0; i < l; i++) {
-    result.push(array1[i], array2[i]);
+function sapXep(s1, s2) {
+    let s1Arr = s1.split("");
+    let s2Arr = s2.split("");
+    let sxArr = [];
+    for (i = 0; i < s2Arr.length; i++) {
+        if (s1Arr[i] != undefined) {
+            sxArr.push(s1Arr[i]);
+        }
+        if (s2Arr[i] != undefined) {
+            sxArr.push(s2Arr[i]);
+        }
+    }
+    console.log(sxArr);
 }
-result.push(...array1.slice(l), ...array2.slice(l));
-
-console.log(result);
+sapXep('super', 'tower');
