73ca7f3c1d3f292dbfadecc3da40fb7d5815f732
Nguyễn Chí Trung Kiên <41412023+ngchitrungkien@users.noreply.github.com>
2020-08-13 21:57:27 +0700

    Add files via upload

--- Ex1.js
+++ Ex1.js
@@ -1,7 +1,19 @@
 function Opposite(firstNum, n) {
     var arr = new Array
-    for (let i = 0; i < n; i++)
-        arr.push((firstNum + i))
+    for (let i = 0; i < n; i++){
+        arr.push(i)
+    }
+    
+    if (firstNum < (n/2)) {
+        console.log("Đối diện với số đầu tiên (" + firstNum + ") là :"
+        + arr[firstNum + (n/2)])
+    } else if (firstNum == (n/2)) {
+        console.log("Đối diện với số đầu tiên (" + firstNum + ") là :"
+        + arr[0])
+    } else {
+        console.log("Đối diện với số đầu tiên (" + firstNum + ") là :"
+        + arr[firstNum - (n/2)])
+    }
 
-    console.log("Đối diện với số đầu tiên (" + firstNum + ") là :" + arr[Math.floor(n / 2)])
 }
+    
